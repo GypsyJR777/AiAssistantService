d77a40bd0b4585222f202336f49d29f689c217c8
gypsyjr <ivan.demidov.01@list.ru>
2025-07-17 00:57:51 +0300

    Change packages + new test assistant

---
+++ ai/src/main/kotlin/com/github/gypsyjr777/ai/service/assistant/BasicSearchAssistant.kt
@@ -0,0 +1,18 @@
+package com.github.gypsyjr777.ai.service.assistant
+
+import dev.langchain4j.service.MemoryId
+import dev.langchain4j.service.SystemMessage
+import dev.langchain4j.service.TokenStream
+import dev.langchain4j.service.UserMessage
+import java.util.UUID
+
+interface BasicSearchAssistant {
+    @SystemMessage(
+        "You are a web search support agent.",
+        "If there is any event that has not happened yet",
+        "You MUST create a web search request with user query and",
+        "use the web search tool to search the web for organic web results.",
+        "Include the source link in your final response."
+    )
+    fun chat(@MemoryId memoryId: UUID, @UserMessage userMessage: String?): TokenStream
+}
